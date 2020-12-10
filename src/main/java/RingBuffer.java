ec32e59d34171b1081cc5f571166ea1381783468
Raffaele Ragni <raffaele.ragni@deliveryhero.com>
2020-12-10 15:41:42 +0100

    cyclic behavior

--- src/main/java/RingBuffer.java
+++ src/main/java/RingBuffer.java
@@ -3,9 +3,13 @@ import java.util.NoSuchElementException;
 public class RingBuffer {
   final int maxSize;
   int size;
+  int[] items;
+  int writePointer;
+  int readPointer;
 
   public RingBuffer(int maxSize) {
     this.maxSize = maxSize;
+    this.items = new int[maxSize];
   }
 
   public boolean isEmpty() {
@@ -24,19 +28,25 @@ public class RingBuffer {
     return size() == maxSize();
   }
 
-  public void addItem(int i) {
+  public void addItem(int item) {
     if(isFull()){
       throw new IllegalStateException("The buffer is full!");
     }
 
     size++;
+    items[writePointer] = item;
+    writePointer++;
+    writePointer = writePointer % 5;
   }
 
   public int getItem() {
-
     if(isEmpty()) {
       throw new NoSuchElementException("The buffer is empty!");
     }
-    return 1;
+    var item = items[readPointer];
+    readPointer++;
+    readPointer = readPointer % 5;
+    size--;
+    return item;
   }
 }
--- src/test/java/RingBufferTest.java
+++ src/test/java/RingBufferTest.java
@@ -35,24 +35,24 @@ class RingBufferTest {
   @Test
   void testAddItem() {
     buffer.addItem(1);
-    
+
     assertFalse(buffer.isEmpty());
     assertThat(buffer.size(), is(1));
   }
-  
+
   @Test
   void testAddMultipleItems() {
     buffer.addItem(1);
     buffer.addItem(2);
-    
+
     assertThat(buffer.size(), is(2));
   }
-  
+
   @Test
   void testGetItem() {
     buffer.addItem(1);
     var item = buffer.getItem();
-    
+
     assertThat(item, is(1));
   }
 
@@ -73,5 +73,36 @@ class RingBufferTest {
     }
     assertThrows(IllegalStateException.class,
             () -> buffer.addItem(6));
+  }
+
+  @Test
+  void testQueueBehavior() {
+    buffer.addItem(1);
+    buffer.addItem(2);
+
+    assertThat(buffer.getItem(), is(1));
+    assertThat(buffer.getItem(), is(2));
+  }
+
+  @Test
+  void testCyclic() {
+    buffer.addItem(1);
+    buffer.addItem(2);
+    buffer.addItem(3);
+    buffer.addItem(4);
+    buffer.addItem(5);
+    assertThat(buffer.getItem(), is(1));
+    assertThat(buffer.getItem(), is(2));
+    assertThat(buffer.getItem(), is(3));
+
+    buffer.addItem(6);
+    buffer.addItem(7);
+    buffer.addItem(8);
+    assertThat(buffer.getItem(), is(4));
+    assertThat(buffer.getItem(), is(5));
+    assertThat(buffer.getItem(), is(6));
+    assertThat(buffer.getItem(), is(7));
+    assertThat(buffer.getItem(), is(8));
+
   }
 }
